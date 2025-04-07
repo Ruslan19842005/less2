65b4af7167889c03538c7c2412bfe33b380ccf3f
yamal <yamal_07@mail.ru>
2025-04-07 14:53:30 +0900

    Zadanie - 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        byte a = 25;
+        System.out.println(a);
+        short b = 270;
+        System.out.println(b);
+        int c = 1000000000;
+        System.out.println(c);
+        long d = 30000000000000000l;
+        System.out.println(d);
+        float e = 2.75f;
+        System.out.println(e);
+        double f = 3.6;
+        System.out.println(f);
+
+
     }
-}
+    }
